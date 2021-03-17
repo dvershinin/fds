3c402b2d4d135c19464cdec1e8d09b34b8cc5d15
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2021-03-17 12:27:52 +0300

    test

--- utils/version-from-tag.sh
+++ utils/version-from-tag.sh
@@ -3,27 +3,27 @@
 # Print what is being run:
 set -x
 
-TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null ||:)
+if TAG=$(git describe --exact-match --tags "$(git log -n1 --pretty='%h')" 2>/dev/null); then
+  VER=$(echo "${TAG}" | grep --perl-regexp --only-matching '\d+\.\d+\.\d+')
+else
+  TAG="v0"
+  VER=0
+fi
+
 PKG=$(basename *.spec .spec)
 
-# If tag looks like a semantic version, then replace version in the spec file
-if $(echo ${TAG} | grep --silent --perl-regexp '^v?\d+\.\d+\.\d+$'); then
-    VER=$(echo ${TAG} | grep --perl-regexp --only-matching '\d+\.\d+\.\d+')
-    if [[ $# -eq 0 ]]; then
-        echo "No arguments supplied, replacing in *.spec"
-        sed -i "s@Version:.*@Version:        ${VER}@" *.spec
-    else
-        sed -i "s@Version:.*@Version:        ${VER}@" $1
-    fi
-else
-    echo "Not a tag checkout. Create github-like archive from current checkout, for rpmbuild to pick that instead of v0.0.1 tag in spec"
-    # by our convention, the spec file in git has Version: 0, so it will extract to <name>-0
-    BUILD_NAME="${PKG}-0"
-    mkdir "/tmp/${BUILD_NAME}"
-    cp -aRp ./* "/tmp/${BUILD_NAME}/"
-    echo "ref-names: HEAD -> master, tag: ${TAG}" > "/tmp/${BUILD_NAME}/.git_archival.txt"
-    pushd /tmp || exit
-    tar -czvf ./v0.tar.gz "./${BUILD_NAME}"
-    popd || exit
-    mv /tmp/v0.tar.gz ./
-fi
+sed -i "s@Version:.*@Version:        ${VER}@" "${PKG}.spec"
+
+echo "Creating github-like archive from current checkout, for rpmbuild"
+# by our convention, the spec file in git has Version: 0, so it will extract to <name>-0
+# first create subst like ref-names: HEAD -> master, tag: v0.0.9 in .git_archival.txt
+# because we are creating archive ourselves
+# Build name is extracted dir name (without v!)
+BUILD_NAME="${PKG}-${VER}"
+mkdir "/tmp/${BUILD_NAME}"
+cp -aRp ./* "/tmp/${BUILD_NAME}/"
+echo "ref-names: HEAD -> master, tag: ${TAG}" > "/tmp/${BUILD_NAME}/.git_archival.txt"
+pushd /tmp || exit
+tar -czvf ./${TAG}.tar.gz "./${BUILD_NAME}"
+popd || exit
+mv /tmp/${TAG}.tar.gz ./
