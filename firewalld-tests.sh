182cf6a2bec5120756f22feedf985ce92d777e7e
Danila Vershinin <dvershinin@cloudlinux.com>
2023-04-22 03:29:17 +0800

    Tests

--- firewalld-tests.sh
+++ firewalld-tests.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 set -x
-# Start the Firewalld service
-systemctl start firewalld
-sleep 5
-
-# Check if Firewalld is running
-if systemctl is-active firewalld; then
-  echo "Firewalld is active"
-else
-  echo "Firewalld is not active"
-  exit 1
-fi
 
 fds block 1.2.3.4
